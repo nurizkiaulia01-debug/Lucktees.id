c04162674fa7770849a525ae3e8410d1a2999275
nurizkiaulia01-debug <nurizkiaulia01@gmail.com>
2026-01-29 11:01:01 +0000

    fix web app

--- web_app.py
+++ web_app.py
@@ -2,7 +2,12 @@ import os
 from flask import Flask, render_template, request, jsonify
 from core import get_bot_reply
 
-app = Flask(__name__, template_folder="templates")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+app = Flask(
+    __name__,
+    template_folder=os.path.join(BASE_DIR, "templates")
+)
 
 @app.route("/")
 def index():
@@ -10,10 +15,7 @@ def index():
 
 @app.route("/chat", methods=["POST"])
 def chat():
-    data = request.get_json() or {}
+    data = request.get_json(silent=True) or {}
     user_message = data.get("message", "")
     reply = get_bot_reply(user_message)
     return jsonify({"reply": reply})
-
-# ❌ JANGAN pakai app.run() di Railway
-# Gunicorn yang akan menjalankan app
