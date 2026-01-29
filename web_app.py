e6299e9218198545ae85d48058428575ce71db2f
nurizkiaulia01-debug <nurizkiaulia01@gmail.com>
2026-01-29 11:20:06 +0000

    fix

--- web_app.py
+++ web_app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Flask hidup di Railway ✅"
+def index():
+    return render_template("index.html")
 
 @app.route("/health")
 def health():
